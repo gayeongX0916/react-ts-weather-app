2e586011d274f07f120a6519932506f9d608a0ed
gayeongX0916 <gayeongx0916@gmail.com>
2025-06-05 15:32:06 +0900

    [feature] 커스텀 훅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,55 +2,23 @@ import "./App.css";
 import SearchSection from "./components/SearchSection";
 import CurrentWeather from "./components/CurrentWeather";
 import WeatherDetails from "./components/WeatherDetails";
-import fetchWeather from "./api/fetchWeather";
 import { useEffect, useState } from "react";
-import type { WeatherKey } from "./types/WeatherKey";
 import fetchPosition from "./api/fetchPosition";
+import useWeather from "./hooks/useWeather";
 
 function App() {
   const [value, setValue] = useState("");
-  const [currentTemp, setCurrentTemp] = useState("0℃");
-  const [currentCity, setCurrentCity] = useState("현재 위치");
-  const [icon, setIcon] = useState("");
-  const [weatherValues, setWeatherValues] = useState<
-    Record<WeatherKey, string>
-  >({
-    rain: "0 mm/h",
-    highTemp: "0℃",
-    lowTemp: "0℃",
-    feelsLike: "0℃",
-    wind: "0 m/s",
-    humidity: "0%",
-  });
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const updateWeather = async (lat: number, lon: number) => {
-    setLoading(true);
-    setError(null);
-
-    fetchWeather(lat, lon)
-      .then((data) => {
-        setWeatherValues({
-          rain: `${data.rain ? data.rain : 0} mm/h`,
-          highTemp: `${(data.main.temp_max - 273.15).toFixed(1)} ℃`,
-          lowTemp: `${(data.main.temp_min - 273.15).toFixed(1)} ℃`,
-          feelsLike: `${(data.main.feels_like - 273.15).toFixed(1)} ℃`,
-          wind: `${data.wind.speed} m/s`,
-          humidity: `${data.main.humidity} %`,
-        });
-        setCurrentTemp(`${(data.main.temp - 273.15).toFixed(1)} ℃`);
-        setCurrentCity(data.name);
-        setIcon(data.weather[0].icon);
-      })
-      .catch((e) => {
-        console.error(e);
-        setError("날씨 정보를 가져오는데 실패했습니다.");
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const {
+    currentTemp,
+    currentCity,
+    icon,
+    weatherValues,
+    updateWeather,
+    loading,
+    error,
+    setError,
+    setLoading,
+  } = useWeather();
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((postion) => {
---
+++ src/hooks/useWeather.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import type { WeatherKey } from "../types/WeatherKey";
+import fetchWeather from "../api/fetchWeather";
+
+type WeatherData = {
+  currentTemp: string;
+  currentCity: string;
+  icon: string;
+  weatherValues: Record<WeatherKey, string>;
+};
+
+const useWeather = () => {
+  const [weatherData, setWeatherData] = useState<WeatherData>({
+    currentTemp: "0℃",
+    currentCity: "현재 위치",
+    icon: "",
+    weatherValues: {
+      rain: "0 mm/h",
+      highTemp: "0℃",
+      lowTemp: "0℃",
+      feelsLike: "0℃",
+      wind: "0 m/s",
+      humidity: "0%",
+    },
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const updateWeather = async (lat: number, lon: number) => {
+    fetchWeather(lat, lon)
+      .then((data) => {
+        setWeatherData({
+          currentTemp: `${(data.main.temp - 273.15).toFixed(1)} ℃`,
+          currentCity: data.name,
+          icon: data.weather[0].icon,
+          weatherValues: {
+            rain: `${data.rain ? data.rain : 0} mm/h`,
+            highTemp: `${(data.main.temp_max - 273.15).toFixed(1)} ℃`,
+            lowTemp: `${(data.main.temp_min - 273.15).toFixed(1)} ℃`,
+            feelsLike: `${(data.main.feels_like - 273.15).toFixed(1)} ℃`,
+            wind: `${data.wind.speed} m/s`,
+            humidity: `${data.main.humidity} %`,
+          },
+        });
+      })
+      .catch((e) => {
+        console.error(e);
+        setError("날씨 정보를 가져오는데 실패했습니다.");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  return {
+    ...weatherData,
+    loading,
+    error,
+    updateWeather,
+    setError,
+    setLoading,
+  };
+};
+
+export default useWeather;
